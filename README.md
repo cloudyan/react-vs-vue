ad3366032ac7a89fccb146ccc01af89829c0ab28
cloudyan <1395093509@qq.com>
2020-09-02 14:06:16 +0800

    feat: add vue-todo-2020

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # react-vs-vue
-react-2020 vs vue-2020 by todo
+
+react-todo-2020 vs vue-todo-2020
+
+## vue-todo-2020
+
+```bash
+vue create vue-todo-2020
+
+# add support less
+npm i less less-loader --save-dev
+
+# add eslint
+```
+
+## react-todo-2020
---
+++ vue-todo-2020/src/components/ToDo.vue
@@ -0,0 +1,162 @@
+<template>
+  <div class="todo">
+    <img class="logo" :src="logo" alt="Vue logo" />
+    <h1 class="todo__header">Vue To Do</h1>
+    <div class="todo__container">
+      <div class="todo__content">
+        <ToDoItem v-for="item in list" :item="item" @delete="onDeleteItem" :key="item.id" />
+      </div>
+      <div class="todo__input">
+        <input
+          type="text"
+          placeholder="I need to..."
+          v-model="todo"
+          v-on:keyup.enter="createNewToDoItem"
+        />
+        <button class="todo__add" @click="createNewToDoItem">+</button>
+      </div>
+      <div class="todo__error-container">
+        <p v-if="showError">Please enter a todo!</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+import ToDoItem from './ToDoItem.vue';
+import Logo from '../assets/logo.png';
+
+export default {
+  name: 'ToDo',
+  components: {
+    ToDoItem
+  },
+  setup() {
+    const list = ref([
+      { id: 1, text: 'clean the house' },
+      { id: 2, text: 'buy milk' }
+    ]);
+    const todo = ref('');
+    const logo = Logo;
+    const showError = ref(false);
+
+    function generateId() {
+      if (list.value && list.value.length) {
+        return Math.max(...list.value.map(t => t.id)) + 1;
+      } else {
+        return 1;
+      }
+    }
+
+    function createNewToDoItem() {
+      //validate todo
+      if (!todo.value) {
+        this.displayError();
+        return;
+      }
+
+      const newId = generateId();
+      list.value.push({ id: newId, text: todo.value });
+      todo.value = '';
+    }
+
+    function onDeleteItem(id) {
+      list.value = list.value.filter(item => item.id !== id);
+    }
+
+    function displayError() {
+      showError.value = true;
+      const clearTimer = setTimeout(() => (showError.value = false), 3000);
+      return () => clearTimeout(clearTimer);
+    }
+
+    return {
+      list,
+      todo,
+      logo,
+      showError,
+      generateId,
+      createNewToDoItem,
+      onDeleteItem,
+      displayError,
+    };
+  }
+};
+</script>
+
+<style lang="less">
+.logo {
+  width: 50px;
+  position: relative;
+  top: 50px;
+}
+
+.todo {
+  text-align: center;
+  width: 80vw;
+  max-width: 768px;
+  height: auto;
+  margin: 40px auto;
+  border-radius: 10px;
+  background: #ffffff;
+
+  .todo__input {
+    margin-top: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  &__header {
+    color: black;
+    font-family: -apple-system, BlinkMacSystemFont, Helvetica Neue, Helvetica,
+      Arial, sans-serif;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin: 70px auto 30px;
+  }
+
+  &__subheader {
+    font-size: 1em;
+    color: black;
+    font-family: -apple-system, BlinkMacSystemFont, Helvetica Neue, Helvetica,
+      Arial, sans-serif;
+    font-weight: 400;
+    margin: 4px auto 20px;
+  }
+
+  &__add {
+    cursor: pointer;
+    width: 50px;
+    height: 50px;
+    border-color: transparent;
+    color: #73ff73;
+    font-size: 2rem;
+    border-radius: 10px;
+    background: linear-gradient(145deg, #e6e6e6, #ffffff);
+    box-shadow: 5px 5px 15px #cccccc, -5px -5px 15px #ffffff;
+  }
+
+  &__add:hover {
+    box-shadow: 5px 5px 10px #cccccc, -5px -5px 10px #ffffff;
+  }
+
+  &__container {
+    width: 80%;
+    margin: 0 auto;
+  }
+
+  &__error-container {
+    height: 100px;
+    width: 90%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: red;
+    font-size: 1rem;
+  }
+}
+
+
+</style>
